a594ca6706993210a91edc04e86020eae3d15184
VenkeyReddy123 <150248945+VenkeyReddy123@users.noreply.github.com>
2025-08-03 14:11:39 +0530

    Update main.py

--- main/main.py
+++ main/main.py
@@ -1,1 +1,10 @@
+from fastapi import FastAPI, Body, Request, HTTPException, UploadFile, File, Form
+app = FastAPI()
 
+@app.get('/')
+async def getallSourcesForApplication(c_id):
+    try:
+        result = "access is suucess fully"
+        return result
+    except:
+        return JSONResponse(content={'error'}, status_code=500)
